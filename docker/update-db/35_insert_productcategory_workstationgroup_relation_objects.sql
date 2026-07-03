499f2d36ec55582e7b18ab36858b7ccfadfc3c6b
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2026-07-03 10:56:09 +0300

    fix: eslestirme pbs_objects id cakismasi giderildi

--- docker/update-db/35_insert_productcategory_workstationgroup_relation_objects.sql
+++ docker/update-db/35_insert_productcategory_workstationgroup_relation_objects.sql
@@ -1,6 +1,6 @@
 INSERT INTO pbs_objects (object_id, object_name, module_id, show_menu, window_type, full_fuseaction, file_path, order_no, is_active) VALUES
-(169, 'Kategori - İş İstasyonu Eşleştirmeleri', 26, true, 'standart', 'production.list_productcategory_workstationgroup_relation', '/production/display/list_productcategory_workstationgroup_relation.cfm', 0, true),
-(170, 'Kategori - İş İstasyonu Eşleştirme Ekle', 26, false, 'standart', 'production.add_productcategory_workstationgroup_relation', '/production/form/add_productcategory_workstationgroup_relation.cfm', 0, true),
-(171, 'Kategori - İş İstasyonu Eşleştirme Kaydet', 26, false, 'standart', 'production.save_productcategory_workstationgroup_relation', '/production/form/save_productcategory_workstationgroup_relation.cfm', 0, true),
-(172, 'Kategori - İş İstasyonu Eşleştirme Sil', 26, false, 'standart', 'production.delete_productcategory_workstationgroup_relation', '/production/form/delete_productcategory_workstationgroup_relation.cfm', 0, true)
+(269, 'Kategori - İş İstasyonu Eşleştirmeleri', 26, true, 'standart', 'production.list_productcategory_workstationgroup_relation', '/production/display/list_productcategory_workstationgroup_relation.cfm', 0, true),
+(270, 'Kategori - İş İstasyonu Eşleştirme Ekle', 26, false, 'standart', 'production.add_productcategory_workstationgroup_relation', '/production/form/add_productcategory_workstationgroup_relation.cfm', 0, true),
+(271, 'Kategori - İş İstasyonu Eşleştirme Kaydet', 26, false, 'standart', 'production.save_productcategory_workstationgroup_relation', '/production/form/save_productcategory_workstationgroup_relation.cfm', 0, true),
+(272, 'Kategori - İş İstasyonu Eşleştirme Sil', 26, false, 'standart', 'production.delete_productcategory_workstationgroup_relation', '/production/form/delete_productcategory_workstationgroup_relation.cfm', 0, true)
 ON CONFLICT (object_id) DO NOTHING;
